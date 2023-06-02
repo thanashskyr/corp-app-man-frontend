ad90a6368fde3c6dc977abd2ceed0ee40024b56a
Thanashs Kyriakopoulos <thanashs.kyr@gmail.com>
2023-06-02 09:12:09 +0300

    test22

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Login from "./login";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Employees from "./Employee";
 import PrivateRoute from "./Auth";
@@ -18,24 +13,26 @@ function App() {
       <div className="body-background">
         <div className="App">
           <div className="content">
-            <Routes>
-              <Route exact path="/corp-app-man-frontend" component={Login} />
-              <PrivateRoute
-                exact
-                path="/corp-app-man-frontend/dashboard"
-                component={Dashboard}
-              />
-              <PrivateRoute
-                exact
-                path="/corp-app-man-frontend/Employee"
-                component={Employees}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path="/corp-app-man-frontend/Department"
-                component={Departments}
-              ></PrivateRoute>
-            </Routes>
+            <Router>
+              <Switch>
+                <Route exact path="/corp-app-man-frontend" component={Login} />
+                <PrivateRoute
+                  exact
+                  path="/corp-app-man-frontend/dashboard"
+                  component={Dashboard}
+                />
+                <PrivateRoute
+                  exact
+                  path="/corp-app-man-frontend/Employee"
+                  component={Employees}
+                ></PrivateRoute>
+                <PrivateRoute
+                  exact
+                  path="/corp-app-man-frontend/Department"
+                  component={Departments}
+                ></PrivateRoute>
+              </Switch>
+            </Router>
           </div>
         </div>
       </div>
